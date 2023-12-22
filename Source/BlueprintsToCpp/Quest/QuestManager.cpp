a2f7a1b93771467a094053df2b86a39f4a7746f4
HadiFirdaus <hadi97firdaus@gmai.com>
2023-12-22 12:27:26 +0800

    Testing commit and push Git to remote repository

--- Source/BlueprintsToCpp/Quest/QuestManager.cpp
+++ Source/BlueprintsToCpp/Quest/QuestManager.cpp
@@ -16,7 +16,7 @@ AQuestManager::AQuestManager()
 void AQuestManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	FunctionToTestGit();
 }
 
 // Called every frame
@@ -43,4 +43,9 @@ void AQuestManager::CompleteQuest_Implementation(FName QuestId, bool CompleteWho
 FQuestInfo AQuestManager::GetQuest(FName Name) const
 {
 	return QuestList[GetQuestIndex(Name)];
+}
+
+void AQuestManager::FunctionToTestGit()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Commit and push to Git repository"));
 }

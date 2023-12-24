ca2f24123cde2406a7c1b2918f834328f4625bc8
HadiFirdaus <hadi97firdaus@gmail.com>
2023-12-25 01:23:20 +0800

    Code Update

--- Source/BlueprintsToCpp/Quest/CompletionTriggers/QuestTriggerCollect.cpp
+++ Source/BlueprintsToCpp/Quest/CompletionTriggers/QuestTriggerCollect.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Quest/CompletionTriggers/QuestTriggerCollect.h"
+#include "Components/SceneComponent.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AQuestTriggerCollect::AQuestTriggerCollect()
@@ -9,6 +11,10 @@ AQuestTriggerCollect::AQuestTriggerCollect()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	RootComponent = SceneRoot;
 }
 
 // Called when the game starts or when spawned
@@ -25,3 +31,7 @@ void AQuestTriggerCollect::Tick(float DeltaTime)
 
 }
 
+bool AQuestTriggerCollect::IsCorrectItem(class AActor* Item)
+{
+	return Item->ActorHasTag(ItemTag);
+}
--- Source/BlueprintsToCpp/Quest/CompletionTriggers/QuestTriggerCollect.h
+++ Source/BlueprintsToCpp/Quest/CompletionTriggers/QuestTriggerCollect.h
@@ -22,4 +22,18 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="QTriggerCollect++")
+	bool IsCorrectItem(class AActor* Item);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class USceneComponent* SceneRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class USphereComponent* SphereCollision;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName QuestID;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName ItemTag;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<AActor*> SeenItems;
 };

3a30c720c4f8c3e4d5156b954b4fe978dabfe5fa
HadiFirdaus <hadi97firdaus@gmail.com>
2024-05-01 18:34:23 +0800

    Transfering codes from QuestManager to QuestManagement

--- Source/BlueprintsToCpp/Core/QuestManagement.h
+++ Source/BlueprintsToCpp/Core/QuestManagement.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+#include "Quest/QuestInfo.h"
 #include "QuestManagement.generated.h"
 
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCompletedQuestSignature, int32, Index);
 /**
  * 
  */
@@ -15,13 +17,27 @@ class BLUEPRINTSTOCPP_API UQuestManagement : public UGameInstanceSubsystem
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category="QuestManagement++")
-	static int32 GetSomeNum();
 
-	UFUNCTION(BlueprintCallable, Category = "QuestManagement++")
-	void SetSomeNum();
+	UFUNCTION(BlueprintCallable)
+	void CompleteQuest(FName QuestId, bool CompleteWholeQuest);
 
-private:
-	static int32 SomeNumStatic;
+	UFUNCTION(BlueprintPure)
+	FQuestInfo GetQuest(FName Name) const;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintPure)
+	bool IsActiveQuest(FName QuestId) const;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintPure)
+	void IsActiveIndex(int32 Index, bool& Active) const;
+
+	//UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	//FCompletedQuestSignature CompletedQuest;
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "C++")
+	TArray<FQuestInfo> QuestList;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintPure)
+	int32 GetQuestIndex(FName QuestId) const;
 	
 };
